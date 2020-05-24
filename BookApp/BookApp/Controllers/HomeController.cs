a3898d02616103122b856e2aea56f30008ed535d
Arvind Gonaki <arvindsg15@gmail.com>
2020-05-25 00:37:00 +0530

    Added new action methos

--- BookApp/BookApp/Controllers/HomeController.cs
+++ BookApp/BookApp/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace BookApp.Controllers
             return View();
         }
 
+        public IActionResult BookPrivacy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
